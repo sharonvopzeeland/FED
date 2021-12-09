46bc131ca8d12670c028005bcb82663e82bc3443
sharonvopzeeland <94179710+sharonvopzeeland@users.noreply.github.com>
2021-12-09 20:35:17 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,33 @@
 // JavaScript Document
 var hamburgerMenu = document.querySelector("nav button");
 
+var colorChangeInput = document.querySelector("header input");
+
+var contactFormKnop = document.querySelector("main section:nth-of-type(8) button");
+
+
 hamburgerMenu.addEventListener("click", toggleHamburger);
 
 function toggleHamburger() {
   var navEl = document.querySelector("nav");
   navEl.classList.toggle("toonHamburger");
+}
+
+
+
+colorChangeInput.addEventListener('change', switchenVanModus);
+
+function switchenVanModus() {
+  if( colorChangeInput.checked ) {
+    document.documentElement.classList.add("darkMode");
+  } else {
+    document.documentElement.classList.remove("darkMode");
+  }
+}
+
+contactFormKnop.addEventListener("click", toggleContact);
+
+function toggleContact() {
+  var formEl = document.querySelector("main section:nth-of-type(8) form");
+  formEl.classList.toggle("toonContact");
 }
